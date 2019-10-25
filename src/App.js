bf29b7fb89bd424e0917640b1d906234836f2e44
ShafoYoisho <muhammadsyafizuraimi@Gmail.com>
2019-10-25 12:17:10 +0800

    frontend yml

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import PokemonCardList from "./components/PokemonCardList";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1 className="header">POKEMON</h1>
+      <h3 className="subHeader">Gotta catch 'em all!</h3>
+      <PokemonCardList />
+    </div>
+  );
 }
 
 export default App;
